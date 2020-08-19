19e7d3d099944b35e385b21edb73a8fbd6397fde
AlokSrivastava <aloksrivastava.0696@gmail.com>
2020-08-19 22:13:39 +0530

    Monthly Wage

--- empwagejava.java
+++ empwagejava.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 import java.util.Random;
 
-public class empwagejava {
+public class emplwagejava {
 
 	public static void main(String[] args) {
 		int EMP_WAGE_PER_HR=20;
 		int EMP_FULL_DAY_HR=8;
 		int EMP_PART_DAY_HR=4;
-		int NO_OF_WORK_DAY=7;
+		int NO_OF_WORK_DAY=20;
 		int totalEmpHr=0;
 		int totalWorkDay=0;
 		int isFullTime=0;
@@ -18,20 +18,15 @@ public class empwagejava {
 		while (totalWorkDay<NO_OF_WORK_DAY) {
 			Random r = new Random();
 			isFullTime=r.nextInt(2);
-			if (isFullTime==1) {
-				EMP_DAY_HR=EMP_FULL_DAY_HR;
-			}
-			else {
-				EMP_DAY_HR=EMP_PART_DAY_HR;
-			}
 			check=attendaceCheck();
-			totalEmpHr+=(check*EMP_DAY_HR);
+			EMP_DAY_HR=(getWorkingHr(check, isFullTime, EMP_FULL_DAY_HR, EMP_PART_DAY_HR));
+			totalEmpHr+=EMP_DAY_HR;
 			System.out.println("Employee wage for Day"+(totalWorkDay+1)+" is ");
 			System.out.println(getDailyEmpWage(totalWorkDay, check, EMP_DAY_HR, EMP_WAGE_PER_HR));
 			totalWorkDay++;
 		}
 		totalWage=(totalEmpHr*EMP_WAGE_PER_HR);
-		System.out.println("Total wage of the employee is:"+totalWage);
+		System.out.println("Total wage of the employee for this month is:"+totalWage);
 	}
 
 	public static int attendaceCheck() {
@@ -41,5 +36,18 @@ public class empwagejava {
 
 	public static int getDailyEmpWage(int tworkday, int attendace, int dayhr, int wagehr) {
 		 return (attendace*dayhr*wagehr);
+	}
+
+	public static int getWorkingHr(int att, int ishr, int fd, int hd) {
+		int empHr=0;
+		switch(ishr) {
+		case 0:
+			empHr=(att*hd);
+			break;
+		case 1:
+			empHr=(att*fd);
+			break;
+		}
+		return empHr;
 	}
 }
